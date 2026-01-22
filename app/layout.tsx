446c6bb45950400b5d0e61e2c3281fa016a26aba
Ibrahim Berk <berkakgun0@gmail.com>
2026-01-23 01:56:50 +0300

    Add Google AdSense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/footer";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -9,11 +10,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1594648180065364"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body>
         <Header />
         {children}
         <Footer />
-
       </body>
     </html>
   );
